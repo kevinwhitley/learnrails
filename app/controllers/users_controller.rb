d9087ea99e6fde97c963b7d4228dcd44a2016b1c
Kevin Whitley <kwnuthatch00@yahoo.com>
2013-05-20 12:09:16 -0700

    user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
+  def new # display form to create new user
+    @user = User.new
+  end
+  def create # respond to new user post
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def show # display an existing user
+    @user = User.find(params[:id])
   end
 end

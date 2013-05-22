76527ce38009aac4c2284245f60d2ed892c0447f
Kevin Whitley <kwnuthatch00@yahoo.com>
2013-05-21 17:39:21 -0700

    completion of user actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      # go wherever the user intended to go (if they weren't signed in)
+      # or else to the user's page
+      redirect_back_or user
     else
       # Create an error message and re-render the signin form.
       # use flash.now instead of flash, because we're doing an internal render
